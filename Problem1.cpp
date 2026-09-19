b927ef8ba7b900e31e215643826629b853774bcf
radtzz <152751850+radtzz@users.noreply.github.com>
2026-09-19 09:48:48 +0700

    Problem1.cpp

--- Problem1.cpp
+++ Problem1.cpp
@@ -1,31 +1,38 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main() {
     int n, k;
     cin >> n >> k;
 
-    vector<int> a;
-    for (int i = 1; i <= n; i++) {
-        a.push_back(i);
+    int* a = new int[n];
+    for (int i = 0; i < n; i++) {
+        a[i] = i + 1;
     }
 
+    int size = n;
     int pos = 0;
 
-    while (a.size() > 1) {
-        pos = (pos + k - 1) % a.size();
-        
+    while (size > 1) {
+        pos = (pos + k - 1) % size;
+
         cout << "astronot ke : " << a[pos] << endl;
 
-        if (a[pos] % 2 == 0) k += 2;
+        int removed_val = a[pos];
+
+        if (removed_val % 2 == 0) k += 2;
         else k -= 1;
         if (k < 2) k = 2;
 
-        a.erase(a.begin() + pos);
+        for (int i = pos; i < size - 1; i++) {
+            a[i] = a[i + 1];
+        }
+        size--;
     }
 
     cout << "astronot ke :" << a[0] << endl;
+
+    delete[] a;
 
     return 0;
 }
